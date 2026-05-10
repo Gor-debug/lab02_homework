656f4e272edcd669821bbb30e3a1976207cb660b
Gor-debug <gorosar06@mail.ru>
2026-05-10 15:08:54 +0300

    added comments

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 
+// Main function: asks user name and prints greeting
 int main()
 {
-    std::string name;
-    std::cout << "Enter name: ";
-    std::cin >> name;
-    std::cout << "Hello world from @" << name << std::endl;
+    std::string name;                 // variable to store user name
+    std::cout << "Enter name: ";      // prompt
+    std::cin >> name;                 // read name
+    std::cout << "Hello world from @" << name << std::endl; // output
     return 0;
 }
